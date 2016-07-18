10f15e67eebebff24d5c87c0e9edc97f52e49c68
Puneet Oberoi <puneet.oberoi@outlook.com>
2016-07-18 19:28:01 -0400

    comments added to server.js

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
+//require/import the HTTP module
 var http = require('http');
 
+//define the ports to listen to
 var PORT1 = 7000;
 var PORT2 = 7500;
 
-function handleRequest(request, response){
-  response.end('It Works!! Path Hit: ' + request.url);
+//function which handles the request and sends response
+function handleRequest1(request, response){
+  response.end('You are the man!');
 }
 
-var server1 = http.createServer(handleRequest);
-var server2 = http.createServer(handleRequest);
+function handleRequest2(request, response){
+  response.end('You suck at this Dude!');
+}
 
+//create server
+var server1 = http.createServer(handleRequest1);
+var server2 = http.createServer(handleRequest2);
+
+
+//start our server and let us know if it is listening
 server1.listen(PORT1, function(){
   console.log('Server listening on: http://localhost:%s', PORT1)
-  console.log('You are the man!!!');
 });
 
 server2.listen(PORT2, function(){
   console.log('Server listening on: http://localhost:%s', PORT2)
-  console.log('You suck at this Dude!!!');
 })
